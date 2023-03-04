19864b8724589c009a2e18895c88a7c767a32c06
holoflash <mikepalacemusic@gmail.com>
2023-03-04 09:14:07 +0100

    Connect frontend to new backend

--- frontend/src/pages/Popwave.js
+++ frontend/src/pages/Popwave.js
@@ -1,11 +1,29 @@
-import { Spotify } from 'react-spotify-embed'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+
 
 const Popwave = () => {
+    const [suggestions, setSuggestions] = useState([]);
+
+    useEffect(() => {
+        const fetchSuggestions = async () => {
+            const response = await axios.get('/api/playlists/popwave');
+            setSuggestions(response.data);
+        }
+        fetchSuggestions();
+    }, []);
+
     return (
         <>
             <h1>popwave</h1>
             <p>Melancholy nostalgia and 80s inspired pop, retrowave, indie rock, synthwave, synthpop, hopebeat. LANY, The Midnight, The 1975, Nightly, MUNA, The Bad Dreamers, Ethel Cain --you get the idea. Carefully curated and updated regularly.</p>
-            <Spotify link="https://open.spotify.com/playlist/36RgZqbsRLfoNmbl3pF8IU?si=eb012e076b4947c8" />
+            <ul>
+                {suggestions.map((suggestion, i) => (
+                    <li key={suggestion.suggestion}>
+                        <strong>{suggestion.suggestion}</strong> - {suggestion.user} ({suggestion.upvotes} upvotes)
+                    </li>
+                ))}
+            </ul>
         </>
     );
 }
