6236e792a1bab7de25c1be7c378836594fa38a33
holoflash <mikepalacemusic@gmail.com>
2023-03-07 08:55:48 +0100

    Add scrollIntoView to pages

--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -3,31 +3,28 @@ import { getAuth, signOut } from "firebase/auth";
 import useUser from './hooks/useUser'
 
 const NavBar = () => {
-
     const { user } = useUser()
     const navigate = useNavigate()
     const location = useLocation()
 
     return (
         <nav>
-            <div className="nav">
-                <div className="nav-section">
-                    <Link to="/">⌂</Link>
-                </div>
-                <div className="nav-section">
-                    <Link to="/soundtrack_aor">SOUNDTRACK AOR</Link>
-                </div>
-                <div className="nav-section">
-                    <Link to="/popwave">popwave</Link>
-                </div>
-                <div className="nav-section">
-                    <Link to="/80s_90s_jazz_fusion">80s & 90s Jazz Fusion/Smooth Jazz</Link>
-                </div>
-                {user
-                    ? <button onClick={() => { signOut(getAuth()) }}>LOG OUT</button>
-                    : <button onClick={() => { navigate('/login', { state: { from: location.pathname } }) }}>LOG IN</button>
-                }
+            <div className="nav-section">
+                <Link to="/">⌂</Link>
             </div>
+            <div className="nav-section">
+                <Link to="/soundtrack_aor">SOUNDTRACK AOR</Link>
+            </div>
+            <div className="nav-section">
+                <Link to="/popwave">popwave</Link>
+            </div>
+            <div className="nav-section">
+                <Link to="/80s_90s_jazz_fusion">80s & 90s Jazz Fusion/Smooth Jazz</Link>
+            </div>
+            {user
+                ? <button onClick={() => { signOut(getAuth()) }}>LOG OUT</button>
+                : <button onClick={() => { navigate('/login', { state: { from: location.pathname } }) }}>LOG IN</button>
+            }
         </nav>
     );
 }
