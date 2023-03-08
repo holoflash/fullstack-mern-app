7a1c0588b0d113dc3ab6b5f485d454dd1b3dec2e
holoflash <mikepalacemusic@gmail.com>
2023-03-08 13:20:44 +0100

    Deploy test

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,11 +20,11 @@ admin.initializeApp({
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get(/^(?!\/api).+/, (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'))
-})
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use(async (req, res, next) => {
     const { authtoken } = req.headers;
