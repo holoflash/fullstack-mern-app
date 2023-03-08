8493a007213df941d29f972d7afe985d370d5ff0
holoflash <mikepalacemusic@gmail.com>
2023-03-08 13:06:53 +0100

    Clean up server.js file

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,8 +10,6 @@ import { ObjectId } from 'mongodb';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-
 const credentials = JSON.parse(
     fs.readFileSync('./credentials.json')
 );
@@ -27,11 +25,6 @@ app.use(express.static(path.join(__dirname, '../build')))
 app.get(/^(?!\/api).+/, (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'))
 })
-
-app.use(express.static(path.join(__dirname, '../build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-});
 
 app.use(async (req, res, next) => {
     const { authtoken } = req.headers;
